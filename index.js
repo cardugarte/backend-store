b74c02970c2cc9a7ea30aeb2c2a6c43aaf9fa95d
Carlos Jesus Perez Dugarte <carlos.perez@vicentelopez.gov.ar>
2021-12-30 10:35:50 -0300

    Se instala faker y se arreglan los params querys

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
 
@@ -7,16 +8,23 @@ app.get('/', (req, resp) => {
 });
 
 app.get('/productos', (req, resp) => {
-  resp.json([
-    {
-    name: 'Sacapuntas',
-    price: 1000
-  },
-    {
-    name: 'Lápiz',
-    price: 1500
-  },
-]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+
+  };
+  resp.json(products);
+});
+
+
+app.get('/productos/filter', (req, resp) => {
+  resp.send('Yo soy un filter');
 });
 
 app.get('/productos/:id', (req, resp) => {
@@ -28,11 +36,24 @@ app.get('/productos/:id', (req, resp) => {
   })
 });
 
+
+app.get('/users', (req, resp) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    resp.json({
+      limit,
+      offset,
+    });
+  } else {
+    resp.send('No existe ningún parámetro');
+  }
+});
+
 app.get('/categorias/:categoryId/productos/:productsId', (req, resp) => {
   const { categoryId, productsId } = req.params;
   resp.json({
     categoryId,
-    productsId
+    productsId,
   })
 })
 
